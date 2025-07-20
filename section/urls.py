b3a360855fb10170a1e71d31f87e1d60deb6f229
Bell <bellenieve@gmail.com>
2025-07-20 21:23:02 +0200

    Initial commit

--- section/urls.py
+++ section/urls.py
@@ -2,6 +2,7 @@ from django .urls import path
 from .import views
 
 urlpatterns = [
-    path('', - views.index_view,name='index')
+    path('', views.index_view,name='index'),
+    path('car/<int:pk>/', views.car_detail_view, name='car_detail'),
 ]
 
--- section/views.py
+++ section/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Car
-
 
 def index_view(request):
     cars = Car.objects.all()
+    return render(request, 'section/index.html', {'cars': cars})
 
-    return render(request, 'section/index.html',{'cars':cars})
+def car_detail_view(request, pk):
+    car = get_object_or_404(Car, pk=pk)
+    return render(request, 'section/car_detail.html', {'car': car})
